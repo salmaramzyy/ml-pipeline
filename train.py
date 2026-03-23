d0461663949300d8cda9e9bc1d62e349afea9c48
salma.wael <Salma.Wael@intercom.com.eg>
2026-03-23 20:12:00 +0200

    fix mlflow path for github actions

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
+mlflow.set_tracking_uri("file:./mlruns")
 mlflow.set_experiment("simple-classifier")
 
 data = load_iris()
